53f3fd3cc4bc7e146972ea20d03e6dd234b365ce
SrushtiPagar <srushtipagar814@gmail.com>
2026-07-20 19:38:01 +0530

    Add DunderX.py

---
+++ Python_Programming/Practice/Python/DunderX.py
@@ -0,0 +1,9 @@
+def main():
+    print("Inside main")
+
+# the Execution is start from the self executable line which is at 0th indentation
+
+#this line is self executable
+if(__name__ == "__main__"):
+    main()
+    print("End of Application")
