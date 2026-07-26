51cee533a5aec36b53d4fb4e3a9da9c23dc2e639
SrushtiPagar <srushtipagar814@gmail.com>
2026-07-26 12:41:03 +0530

    Add CommandLine1.py

---
+++ Python_Programming/Practice/Python/CommandLine1.py
@@ -0,0 +1,11 @@
+#program input is given after running the code
+
+print("Enter Fisrt Number : ")
+No1 = int(input())
+
+print("Enter Second Number : ")
+No2 = int(input())
+
+Ans = No1 +No2
+
+print("Addition is : ", Ans)
