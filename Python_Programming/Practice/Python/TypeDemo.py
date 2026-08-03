4fe3ed95d552ef758ea77b1c151580296f886333
Srushti Nitin Pagar <srushtipagar814@gmail.com>
2026-08-03 21:48:05 +0530

    Add files via upload

---
+++ Python_Programming/Practice/Python/TypeDemo.py
@@ -0,0 +1,18 @@
+# Number
+no = 11             # int
+Marks = 90.78       # float
+Value = 8+5j        # complex
+
+print(Value)
+
+print(type(no))
+print(type(Marks))
+print(type(Value))
+
+gender = True       #boolean
+print(gender)
+print(type(gender))
+
+Demo = None
+print(Demo)
+print(type(Demo))
