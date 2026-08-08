f68b48d8c4af22fa493702fbc604c16dcd332316
Srushti Nitin Pagar <srushtipagar814@gmail.com>
2026-08-08 23:01:49 +0530

    Add files via upload

---
+++ Python_Programming/Practice/Python/AdditionTypeCast.py
@@ -0,0 +1,11 @@
+print("Enter First Number : ")
+No1 = input()
+
+print("Enter Second Number : ")
+No2 = input()
+
+#Ans = (int)No1 + (int)No2
+
+Ans = int(No1) + int(No2)
+
+print("Addition is : ",Ans)
