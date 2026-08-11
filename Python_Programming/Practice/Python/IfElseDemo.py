d04fa6a0efa342ff0cfae8e11c36ff47d9266cf2
Srushti Nitin Pagar <srushtipagar814@gmail.com>
2026-08-11 22:57:45 +0530

    Add files via upload

---
+++ Python_Programming/Practice/Python/IfElseDemo.py
@@ -0,0 +1,9 @@
+#programming paradigms - Sequence + Selection (if else)
+
+print("Enter your Age : ")
+Age = int(input())
+
+if (Age < 18):
+    print("Sorry, you are no allowed")
+else : 
+    print("Welcome to the movie")
