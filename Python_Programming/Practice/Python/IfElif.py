242450ea156c6962eb59bfa16fae5dd7f474f4d8
Srushti Nitin Pagar <srushtipagar814@gmail.com>
2026-08-11 22:58:10 +0530

    Add files via upload

---
+++ Python_Programming/Practice/Python/IfElif.py
@@ -0,0 +1,16 @@
+#print("-"*40)
+print("------------------------------------------------")
+print("------------Ticket Pricing Software-------------")
+print("------------------------------------------------")
+
+print("Please enter your age : ")
+Age = int(input())
+
+if(Age <= 5):
+    print("FREE Entry")
+elif(Age > 5 and Age <= 18):
+    print("Ticket Price : 900")
+elif(Age > 18 and Age <= 40):
+    print("Ticket Price : 1200")
+else:
+    print("Ticket Price : 500")
