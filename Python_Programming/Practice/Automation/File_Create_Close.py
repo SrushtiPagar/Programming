978ac2e8ea2362159ae3c0dbde7853115270512e
SrushtiPagar <srushtipagar814@gmail.com>
2026-08-14 12:38:01 +0530

    Add File_Create_Close.py

---
+++ Python_Programming/Practice/Automation/File_Create_Close.py
@@ -0,0 +1,12 @@
+def main():
+    try :
+        fobj = open("Demo.txt","w")
+        print("file gets opened")
+
+        fobj.close()
+
+    except FileNotFoundError as fobj:
+        print("File is not Present in Current Directory")
+
+if(__name__ == "__main__"):
+    main()
