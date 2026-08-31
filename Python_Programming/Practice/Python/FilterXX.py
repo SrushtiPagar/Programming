ad43b167b16da17eb665484b87e3770a03483e69
SrushtiPagar <srushtipagar814@gmail.com>
2026-08-31 22:29:01 +0530

    Add FilterXX.py

---
+++ Python_Programming/Practice/Python/FilterXX.py
@@ -0,0 +1,15 @@
+CheckEven = lambda No: (No%2 == 0)
+
+def main():
+    Data = [13,12,8,10,11,20]
+
+    print("Input Data is : ",Data)
+
+    #function passed to filter should always return true or false
+    #Iterable = filer(Iterable(list),Function)
+    FData = list(filter(CheckEven,Data))
+
+    print("Data After Filter : ",FData)
+
+if(__name__ == "__main__"):
+    main()
