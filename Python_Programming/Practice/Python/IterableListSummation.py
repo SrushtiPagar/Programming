208e176a1aee73a0228aa3052b8bf48831101a0c
SrushtiPagar <srushtipagar814@gmail.com>
2026-09-12 21:54:24 +0530

    Add IterableListSummation.py

---
+++ Python_Programming/Practice/Python/IterableListSummation.py
@@ -0,0 +1,20 @@
+# Ordered
+# Indexed
+# Mutable
+# allow duplicates
+
+def main():
+    Marks = [78,90,56,98,77]
+
+    for no in Marks :
+        print(no)
+
+    Marks[2] = 59               #Multability
+
+    print("-"*15)
+
+    for no in Marks :
+        print(no)
+
+if(__name__ == "__main__"):
+    main()
