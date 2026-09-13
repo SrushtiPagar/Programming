753ddf9118113b56e2a5bdfc145551afd28b1f1e
SrushtiPagar <srushtipagar814@gmail.com>
2026-09-13 21:56:34 +0530

    Add ListTravelX.py

---
+++ Python_Programming/Practice/Python/ListTravelX.py
@@ -0,0 +1,9 @@
+def main():
+    Marks = [78,90,45,65]
+
+    for i in range(len(Marks))  :
+        print(Marks[i])
+        #print(i)           #this only prints 0 1 2 3
+
+if(__name__ == "__main__") :
+    main()
