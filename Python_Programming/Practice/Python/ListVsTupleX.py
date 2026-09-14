a73f163223ac7250b5688e3dcb580a5fa023d297
SrushtiPagar <srushtipagar814@gmail.com>
2026-09-14 21:57:15 +0530

    Add ListVsTupleX.py

---
+++ Python_Programming/Practice/Python/ListVsTupleX.py
@@ -0,0 +1,22 @@
+#=============================================
+#                   List        Tuple
+#==============================================
+#Ordered            yes         yes
+#Indexed            yes         yes
+#Mutable            yes         no
+#Heterogenity       yes         yes
+#==============================================
+
+
+def main():
+    Data1 = [10,3.14,True,"Pune"]               #   list
+    Data2 = (10,3.14,True,"Pune")               #   Tuple
+
+    print(Data1)
+    print(Data2)
+
+    print(Data1[0])
+    print(Data2[0])
+
+if(__name__ == "__main__") :
+    main()
